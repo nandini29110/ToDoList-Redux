4dbabc7744084f34d5a5d46b933dba32cee95910
Nandini Sharma <nandini29cs@gmail.com>
2021-08-25 23:09:43 +0530

    basic structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Todo from "./components/Todo.js";
+
+function App() {
+  return (
+    <>
+    <Todo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./todo.css";
+
+const Todo=()=>{
+   return(
+       <>
+             <div className="main-div">
+    <div className="child-div">
+    <figure>
+      <figcaption>Add Your List Here ...</figcaption>
+    </figure>
+
+   <div>
+   <input type="text" placeholder=" Add Items ..."/>
+   <i></i>
+   </div>
+    </div>
+    </div>
+       </>
+   );
+}
+
+export default Todo;
